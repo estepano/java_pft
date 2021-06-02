c10b4d577744c5a76323fdce6666c85c60c3c8e4
estepano <82460376+estepano@users.noreply.github.com>
2021-06-02 23:05:31 +0300

    Первый автотест: вспомогательные методы и классы добавлены

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupData {
+    private final String name;
+
+    public GroupData(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
